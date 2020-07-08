885ea91a6a0b9bd9208488d364cfd6921505f34f
krishnammal <krishnammal.r@yahoo.com>
2020-07-08 19:11:36 +0530

    First commit

---
+++ src/main/java/com/sample/microservices/springClouldConfigServer/SortNumbers.java
@@ -0,0 +1,15 @@
+package com.sample.microservices.springClouldConfigServer;
+
+public class SortNumbers {
+
+	
+	public static void main(String[] s){
+		
+		int[] a = {1,0,5,3,2};
+		
+		int[] sortedArr =  new int[a.length];
+		for(int i = 0; i < a.length ; i++){
+			
+		}
+	}
+}
